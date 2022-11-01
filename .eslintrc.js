2d06103094a7efc9c4c34d623cb15509122671e7
awran5 <awran5@yahoo.com>
2022-11-01 10:53:18 +0200

    style: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,30 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true
+    browser: true, // Enables browser globals like window and document
+    es2021: true,
+    node: true // Enables Node.js global variables and Node.js scoping.
   },
-  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', 'airbnb', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module', // Allows using import/export statements
     ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
-    sourceType: 'module'
+      jsx: true // Enable JSX since we're using React
+    }
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     'import/extensions': [
       'error',
       'always',
@@ -29,13 +39,16 @@ module.exports = {
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'no-use-before-define': 'off',
     'no-unused-vars': 'warn',
-    'import/prefer-default-export': 1,
+    'import/prefer-default-export': 0,
     'no-shadow': 1,
     'prefer-const': 1,
     'prefer-spread': 1,
     'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }]
   },
   settings: {
+    react: {
+      version: 'detect' // Automatically detect the react version
+    },
     'import/resolver': {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx']
